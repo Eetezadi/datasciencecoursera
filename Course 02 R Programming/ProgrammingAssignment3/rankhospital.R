93113c4ec67e3a524e24273bb76e317da3338c44
Sina Eetezadi <code@eetezadi.com>
2015-08-19 21:02:43 -0400

    Fix rankhospiral.R using dplyr

--- Course 02 R Programming/ProgrammingAssignment3/rankhospital.R
+++ Course 02 R Programming/ProgrammingAssignment3/rankhospital.R
@@ -24,10 +24,12 @@
 # with the exact message “invalid outcome”.
 
 
-# Determines the best state for each outcome
+# Determines the hospital for each outcome and state at a specific ranking
+# Dependencies: dplyr
 # Arguments:
 #   state: character: 2 Letter code for the state
 #   outcome: factor: heart attack”, “heart failure” or “pneumonia”
+#   num: integer, ranking of the hospital, "best" | "worst" also allowed
 
 rankhospital <- function(state, outcome, num = "best"){
   
@@ -35,38 +37,37 @@ rankhospital <- function(state, outcome, num = "best"){
   data = read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
   
   # Create vectors with valid states, valid outcomes and cols
-  valid_states <- levels(as.factor(data[,7]))
-  valid_outcomes <- c("heart attack", "heart failure","pneumonia")
-  outcome_cols = c(11, 17, 23); # from manual in order
+  o_valid <- data.frame(input = c("heart attack", "heart failure", "pneumonia"),
+                        colno = c(11, 17, 23),
+                        colname = c("Heart.Attack", "Heart.Failure", "Pneumonia"))
+  s_valid <- data %>% distinct(State) %>% select(State)
   
-  # Throw if not valid state
-  if (!(state %in% valid_states)) stop("invalid state")
-  if (!(outcome %in% valid_outcomes)) stop("invalid outcome")
+  # Throw if not valid state or valid outcome
+  if (!(state %in% s_valid$State)) stop("invalid state")
+  if (!(outcome %in% o_valid$input)) stop("invalid outcome")
   
-  # Determine column number for outcome
-  outcomes_col <- outcome_cols[match(outcome, valid_outcomes)]
   
-  # Select only rows with state and no "Not Available"
-  rcond <- data[,7] == state & !(data[, outcomes_col] == "Not Available")
-  # Extract hospital.name and outcomes column.
-  death_rates <- data[rcond, c(2, outcomes_col)]
+  # Creating the right column name based on outcome
+  outcome_col = paste0("Hospital.30.Day.Death..Mortality..Rates.from.",
+                       o_valid$colname[which(o_valid$input == outcome)])
   
-  # Order the selected hospitals by rank
-  ranked_hospitals <- death_rates[with(death_rates, 
-                                       order(as.numeric(death_rates[,2]),
-                                             death_rates[,1])),] 
-  
-  # Rewrite num
-  no_hospitals <- length(ranked_hospitals[,1])
-  if (num == "best") num <- 1
-  else if (num == "worst") num <- as.numeric(no_hospitals)
-  
-  # Validate num for integer, length
-  if (class(num) != "numeric") stop("invalid num")
-  else if (num %% 1 != 0) stop("invalid num")
-  else if (num > no_hospitals | num < 1) return("NA")
+  # dplyr pipes action: Select and rename columns, filter for state and invalid 
+  # values, convert to number and finally arrange by outcome and break ties with
+  # hospital name
+  mort_df <- data %>% select_(hospital = "Hospital.Name", 
+                              outcome = outcome_col,
+                              states = "State") %>% 
+                      filter(states == state & outcome != "Not Available") %>%
+                      mutate(outcome = as.numeric(outcome)) %>%
+                      arrange(outcome, hospital)
 
   
-  # Return best hospital that comes first in alphabet
-  return(ranked_hospitals[num,1])
+  # Rewrite num
+  if (num == "best") num <- 1
+  else if (num == "worst") num <- length(mort_df$hospital)
+  
+
+  # Return hospital according to ranking if ranking exists
+  if (is.na(mort_df$hospital[num])) return("NA")
+  else return(mort_df$hospital[num])
 }
